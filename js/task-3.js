d1cec046383f6c752a905475bbea591cc6ae49e0
Habl <habltaker@gmail.com>
2025-05-11 12:35:19 +0300

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth (content, padding, border) {
-    return parseFloat(content) + parseFloat(padding) + parseFloat(border);
+    return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
